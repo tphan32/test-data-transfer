83d34edc82a2a9e445fbb12a05658e45ba7cd4e1
Tinh Phan <pttinh0804@gmail.com>
2024-03-26 23:51:21 +0700

    chore: logging

--- app/actions/download.js
+++ app/actions/download.js
@@ -2,6 +2,7 @@
 
 import { downloadFileFromAzure } from "@tphan32/data-transfer";
 export default async function downloadFile(fileName) {
+  console.log("downloadFile", fileName);
   const file = await downloadFileFromAzure(fileName);
   // console.log("downloadFile", file);
   return file;
--- app/actions/upload.js
+++ app/actions/upload.js
@@ -2,6 +2,9 @@
 
 import { uploadFileToAzure } from "@tphan32/data-transfer";
 export default async function uploadFile(file, fileName) {
+  console.log("process.env", process.env.AZURE_STORAGE_CONNECTION_STRING);
+  console.log("process.env", process.env.AZURE_STORAGE_CONTAINER_NAME);
+  console.log("uploadFile", file, fileName);
   const name = await uploadFileToAzure(file, fileName);
   return name;
 }
